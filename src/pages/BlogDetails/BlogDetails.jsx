5e266afac6ea6a16bc4a123c7ca0192de44c4384
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-28 10:46:07 -0700

    set up useEffect for blog details

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,4 +1,24 @@
-const BlogDetails = () => {
+// npm modules
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+// services
+import * as BlogService from '../../services/BlogService'
+
+const BlogDetails = (props) => {
+  const { blogId } = useParams()
+  const [blog, setBlog] = useState(null)
+
+  useEffect(() => {
+    const fetchBlog = async() => {
+      const blogData = await BlogService.show(blogId)
+      setBlog(blogData)
+    }
+    fetchBlog()
+  }, [blogId])
+
+console.log('Blog State: ', blog)
+
   return (  
     <main>
       <h1>Blog Details Page</h1>
