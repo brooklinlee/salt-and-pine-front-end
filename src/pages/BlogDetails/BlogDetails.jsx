3231851eb328093ae1d4b03f790aba9a18559a8a
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-28 10:55:03 -0700

    set up blog details page

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -5,7 +5,11 @@ import { useParams } from "react-router-dom"
 // services
 import * as BlogService from '../../services/BlogService'
 
-const BlogDetails = (props) => {
+// components
+import Loading from '../../components/Loading/Loading'
+import AuthorInfo from "../../components/AuthorInfo/AuthorInfo"
+
+const BlogDetails = () => {
   const { blogId } = useParams()
   const [blog, setBlog] = useState(null)
 
@@ -17,11 +21,28 @@ const BlogDetails = (props) => {
     fetchBlog()
   }, [blogId])
 
-console.log('Blog State: ', blog)
+// console.log('Blog State: ', blog)
+
+if (!blog) return <Loading />
 
   return (  
     <main>
       <h1>Blog Details Page</h1>
+      <article>
+        <h1>{blog.title}</h1>
+        {/* <h3>{blog.author}</h3> */}
+        <AuthorInfo content={ blog }/>
+        <h3>{blog.location}</h3>
+        <h3>{blog.category}</h3>
+        <p>{blog.text}</p>
+      </article>
+      <section>
+      <button>Like</button>
+      <button>Save</button>
+      </section>
+      <section>
+        <h3>Comments</h3>
+      </section>
     </main>
   )
 }
