96e0d272d673c70db4a4601791d8121342498905
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-12-05 15:59:11 -0700

    write new vlog form

--- src/pages/NewVlog/NewVlog.jsx
+++ src/pages/NewVlog/NewVlog.jsx
@@ -31,11 +31,47 @@ const NewVlog = (props) => {
     <label htmlFor="title-input">Title: </label>
     <input 
       required
-      onChange={handleChange}
       type="text" 
       name='title'
       id='title-input'
       placeholder="Title"
+      value={formData.title}
+      onChange={handleChange}
+    />
+    <label htmlFor="text-input">Text: </label>
+    <textarea  
+      required 
+      name="text" 
+      id="text-input" 
+      cols="30" rows="10" 
+      onChange={handleChange}
+      value={formData.text}
+      placeholder="About this Vlog...">
+    </textarea>
+    <label htmlFor="category-input">Category: </label>
+    <select 
+      required
+      name="category" 
+      id="category-input"
+      value={formData.category}
+      onChange={handleChange}
+    >
+      <option value="Utah Gems">Utah Gems</option>
+      <option value="Great Outdoors">Great Outdoors</option>
+      <option value="Foodie Finds">Foodie Finds</option>
+      <option value="Travel">Travel</option>
+      <option value="Events & Festivals">Events & Festivals</option>
+      <option value="Seasonal">Seasonal</option>
+      <option value="Personal Stories">Personal Stories</option>
+    </select>
+    <label htmlFor="location-input">Location: </label>
+    <input 
+      required
+      type="text" 
+      name='location'
+      id='location-input'
+      value={formData.location}
+      onChange={handleChange}
     />
     {/* submit button */}
     <button type="submit">Add New Vlog</button>
